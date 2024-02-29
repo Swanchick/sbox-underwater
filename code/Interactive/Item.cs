21eea83d59b82147e0f120729d87858a953b4811
Swanchick <61909965+Swanchick@users.noreply.github.com>
2024-02-29 21:41:10 +0200

    Now inventory is fully works

--- code/Interactive/Item.cs
+++ code/Interactive/Item.cs
@@ -5,6 +5,9 @@ public class Item : BaseInteractive
 {
 	[Property] public string ImagePath { get; set; } = "/Textures/Items/iron.png";
 
+	protected virtual bool IsInInventory { get; set; } = false;
+	public bool IsCurrentItem { get; set; } = false;
+
 	private Rigidbody rigidBody;
 	private BoxCollider collider;
 	private ModelRenderer modelRenderer;
@@ -18,27 +21,61 @@ public class Item : BaseInteractive
 		Network.SetOwnerTransfer( OwnerTransfer.Takeover );
 	}
 
-	private void OnTake()
+	protected virtual void MakeItemForSlot(bool pleaseDontMake)
 	{
-		rigidBody.Gravity = false;
-		collider.Enabled = false;
-		modelRenderer.Enabled = false;
+		rigidBody.Gravity = pleaseDontMake;
+		collider.Enabled = pleaseDontMake;
+		modelRenderer.Enabled = pleaseDontMake;
 	}
 
-	public void Drop()
+	protected virtual void OnTake(Guid playerId)
 	{
+		if ( IsInInventory )
+			return;
 		
+		GameObject playerObject = FindPlayer( playerId );
+		PlayerInventory inventory = playerObject.Components.Get<PlayerInventory>();
+
+		MakeItemForSlot(false);
+
+		IsInInventory = true;
+		IsInteractive = false;
+
+		inventory.Take( this );
+	}
+
+	protected override void OnUpdate()
+	{
+		Controlls();
+	}
+
+	protected virtual void Controlls()
+	{
+		if ( IsCurrentItem )
+			return;
+	}
+
+	[Broadcast]
+	public virtual void Drop(Vector3 forward, float throwPower)
+	{	
+		if ( !IsInInventory )
+			return;
+
+		IsInInventory = false;
+		IsCurrentItem = false;
+
+		IsInteractive = true;
+
+		MakeItemForSlot( true );
+
+		Network.DropOwnership();
+
+		rigidBody.Velocity = forward * throwPower;
 	}
 
 	[Broadcast]
 	public override void OnInteract( Guid playerId )
 	{
-		GameObject playerObject = FindPlayer( playerId );
-		PlayerInventory inventory = playerObject.Components.Get<PlayerInventory>();
-
-		OnTake();
-
-		IsInteractive = false;
-		inventory.Take( this );
+		OnTake(playerId);
 	}
 }
--- code/Player/PlayerInventory.cs
+++ code/Player/PlayerInventory.cs
@@ -4,6 +4,9 @@ using System;
 public class PlayerInventory : Component
 {
 	[Property] public GameObject inventoryStorage;
+	[Property] public CameraComponent playerCamera;
+
+	[Property] public float ThrowPower = 250f;
 
 	public List<Item> Inventory { get; set; } = new();
 	public int MaxSlots { get; set; } = 4;
@@ -13,7 +16,7 @@ public class PlayerInventory : Component
 
 	private float currentSlot = 0;
 	
-	public void Take(Item item)
+	public virtual void Take(Item item)
 	{
 		GameObject itemObject = item.GameObject;
 		itemObject.Network.TakeOwnership();
@@ -21,13 +24,70 @@ public class PlayerInventory : Component
 		itemObject.Transform.LocalPosition = Vector3.Zero;
 
 		Inventory.Add(item);
-
 		OnItemAdded?.Invoke( Inventory );
+
+		if ((int)currentSlot == Inventory.Count - 1 )
+		{
+			SetCurrentItem( (int)currentSlot );
+		}
+	}
+
+	public virtual void Drop()
+	{
+		Item item = GetItem( (int)currentSlot );
+		if ( item is null )
+			return;
+
+		Inventory.Remove( item );
+		OnItemAdded?.Invoke( Inventory );
+
+		item.Drop(playerCamera.Transform.Rotation.Forward, ThrowPower);
+
+		GameObject itemObject = item.GameObject;
+		itemObject.SetParent( Scene );
+		itemObject.Transform.Position = playerCamera.Transform.Position;
 	}
 
 	protected override void OnUpdate()
 	{
 		ChangeSlot();
+		ChangeSlotByKeyboards();
+		Controlls();
+	}
+	
+	private void Controlls()
+	{
+		if ( IsProxy )
+			return;
+
+		if ( Input.Pressed("Drop") )
+		{
+			Drop();
+		}
+	}
+
+	private void SetCurrentItem(int slot)
+	{
+		foreach (Item _item in Inventory )
+		{
+			_item.IsCurrentItem = false;
+		}
+
+		if ( slot > Inventory.Count - 1 )
+			return;
+
+		Item item = Inventory[slot];
+		item.IsCurrentItem = true;
+
+		Log.Info( "Item changed" );
+	}
+	
+	private Item GetItem(int slot)
+	{
+		if ( slot > Inventory.Count - 1 )
+			return null;
+
+		return Inventory[slot];
 	}
 
 	private void ChangeSlot()
@@ -37,16 +97,30 @@ public class PlayerInventory : Component
 
 		float newSlot = currentSlot;
 
-		newSlot += Input.MouseWheel.y * 0.5f;
+		newSlot -= Input.MouseWheel.y * 0.5f;
 		newSlot = Math.Clamp( newSlot, 0, MaxSlots - 1 );
 
 		if ( newSlot != currentSlot )
 		{
-			Log.Info( "Slot has been changed" );
-
-			OnSlotChanged?.Invoke((int)newSlot);
+			SetCurrentItem( (int)newSlot );
+			OnSlotChanged?.Invoke( (int)newSlot );
 		}
 
 		currentSlot = newSlot;
+	}
+
+	private void ChangeSlotByKeyboards()
+	{
+		if ( IsProxy )
+			return;
+
+		for (int slot = 0; slot < MaxSlots; slot++ )
+		{	
+			if (Input.Pressed($"Slot{slot + 1}" ))
+			{
+				SetCurrentItem( slot );
+				OnSlotChanged?.Invoke(slot);
+			}
+		}
 	}
 }
